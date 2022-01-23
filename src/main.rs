c4ba49080fba46ea25827f2660c0451ed487f8bc
Connor Mooneyhan <connmoo@gmail.com>
2022-01-23 18:53:44 -0500

    Add back hello_world

--- src/main.rs
+++ src/main.rs
@@ -9,16 +9,25 @@ struct Name(String);
 fn main() {
     App::new()
         .add_startup_system(add_people)
-        .add_system(hello_world)
+        .add_system(greet_people)
         .run();
 }
 
-fn hello_world() {
-    println!("hello world!");
+fn greet_people(query: Query<&Name, With<Person>>) {
+    query.iter().for_each(|name| println!("Hello, {}!", name.0));
 }
 
 fn add_people(mut commands: Commands) {
-    commands.spawn().insert(Person).insert(Name("Elaina Proctor".to_string()));
-    commands.spawn().insert(Person).insert(Name("Renzo Hume".to_string()));
-    commands.spawn().insert(Person).insert(Name("Zayna Nieves".to_string()));
+    commands
+        .spawn()
+        .insert(Person)
+        .insert(Name("Elaina Proctor".to_string()));
+    commands
+        .spawn()
+        .insert(Person)
+        .insert(Name("Renzo Hume".to_string()));
+    commands
+        .spawn()
+        .insert(Person)
+        .insert(Name("Zayna Nieves".to_string()));
 }
